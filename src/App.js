37b3eff9addbb91882a4960a54bb1c21f7ccaf34
ParmezhovSergey <parmezhovsergey@mail.ru>
2022-07-03 11:09:17 +0300

    5

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/episode-list" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
